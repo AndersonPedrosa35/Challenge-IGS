359227a17b98a1017a618b04ff6613ce1917030b
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2022-01-30 12:05:08 -0300

    Inserir - Header.js - Criaçao de uma logica de renderizaçao diferente para mobile e desktop, utilizaçao de um menu hamburguer no mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,8 @@ import verSenha from "../assets/Archive/eye_idle.png";
 import naoVerSenha from "../assets/Archive/eye_off_idle.png";
 import userImage from "../assets/Archive/user_idle.png";
 import help from "../assets/Archive/help_idle.png";
-import logout from "../assets/Archive/logout_idle.png"
+import logout from "../assets/Archive/logout_idle.png";
+import menu from "../assets/navbar.svg";
 
 function seeMoney(state) {
   if (state === true) {
@@ -25,13 +26,40 @@ function seeMoney(state) {
   )
 }
 
+function detectar_mobile() { 
+  if( navigator.userAgent.match(/Android/i)
+  || navigator.userAgent.match(/webOS/i)
+  || navigator.userAgent.match(/iPhone/i)
+  || navigator.userAgent.match(/iPad/i)
+  || navigator.userAgent.match(/iPod/i)
+  || navigator.userAgent.match(/BlackBerry/i)
+  || navigator.userAgent.match(/Windows Phone/i)
+  ){
+     return true;
+   }
+  else {
+     return false;
+   }
+ }
+
 export default function Header() {
   const [see, setSee] = useState(false);
   const [isHelp, setIsHelp] = useState(false);
 
   return (
     <header className='header-image'>
-      <section className='header-container-left'>
+      { detectar_mobile() === true ? (
+        <section className='header-container-left'>
+           <button
+            type="button"
+            className="button-icon"
+            onClick={ () => setIsHelp(!isHelp)}
+          >
+            <img src={ menu } alt="Menu" width={ "40px" } />
+          </button>
+        </section>
+      ) : (
+        <section className='header-container-left'>
         <button
           type="button"
           className="button-icon"
@@ -54,6 +82,8 @@ export default function Header() {
           <img src={ userImage } alt="Sair da conta" width={ "50px" } />
         </button>
       </section>
+      ) }
+      
       <section className="header-container-center">
         <h5>JACKPOT</h5>
         <span>$</span>
