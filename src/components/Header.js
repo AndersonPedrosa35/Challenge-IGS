720570741e265b1c2c59389779b3256559a1cd9d
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2022-01-30 11:19:26 -0300

    Inserir - Styles - Estilizando a imagem header.png, posicionando elementos, retirando bordas, backgrounds, entre outras propriedades de fundo, refatorando o header apos essa mudança

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,68 +1,78 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import verSenha from "../assets/Archive/eye_idle.png";
 import naoVerSenha from "../assets/Archive/eye_off_idle.png";
+import userImage from "../assets/Archive/user_idle.png";
 import help from "../assets/Archive/help_idle.png";
 import logout from "../assets/Archive/logout_idle.png"
 
 function seeMoney(state) {
-    if (state === true) {
-      return (
-        <>
-          <span>$</span>
-          <strong>0000,00</strong>
-          <img src={ verSenha } alt="seeMoney" width={ "35px" } /> 
-        </>
-      )
-    }
+  if (state === true) {
     return (
       <>
         <span>$</span>
-        <span>______________</span>
-        <img src={ naoVerSenha } alt="notSeeMoney" width={ "35px" } /> 
+        <strong>0000,00</strong>
+        <img src={ verSenha } alt="seeMoney" width={ "35px" } /> 
       </>
     )
+  }
+  return (
+    <>
+      <span>$</span>
+      <span>______________</span>
+      <img src={ naoVerSenha } alt="notSeeMoney" width={ "35px" } /> 
+    </>
+  )
 }
 
 export default function Header() {
-    const [see, setSee] = useState(false);
+  const [see, setSee] = useState(false);
+  const [isHelp, setIsHelp] = useState(false);
 
   return (
-    <header className='header'>
-        <section className='header-container-left'>
-				  <button
-						type="button"
-						className="button-icon"
-						onClick={ () => console.log("CLICOU") }
-					>
-            <img src={ logout } alt="Sair da conta" width={ "50px" } />
-					</button>
-					<button
-						type="button"
-						className="button-icon"
-						onClick={ () => console.log("CLICOU")}
-					>
-            <img src={ help } alt="Ajuda" width={ "50px" } />
-					</button>
-        </section>
-        <section className="header-container-center">
-          <h5>jackpot</h5>
-          <span>$</span>
-          <h3 className="jackpot">
-            26
-            <small>.</small>
-            000<small>.</small>
-            000
-          </h3>
-        </section>
+    <header className='header-image'>
+      <section className='header-container-left'>
+        <button
+          type="button"
+          className="button-icon"
+          onClick={ () => setIsHelp(!isHelp)}
+        >
+          <img src={ help } alt="Ajuda" width={ "50px" } />
+        </button>
+        <button
+          type="button"
+          className="button-icon"
+          onClick={ () => <Link to="home" /> }
+        >
+          <img src={ logout } alt="Sair da conta" width={ "50px" } />
+        </button>
+        <button
+          type="button"
+          className="button-icon"
+          onClick={ () => <Link to="home" /> }
+        >
+          <img src={ userImage } alt="Sair da conta" width={ "50px" } />
+        </button>
+      </section>
+      <section className="header-container-center">
+        <h5>JACKPOT</h5>
+        <span>$</span>
+        <h3 className="jackpot">
+          12
+          <small>.</small>
+          800<small>.</small>
+          000
+        </h3>
+      </section>
       <section className='header-container-right'>
-            <button
-              type="button"
-							className="button-icon container-money"
-              onClick={ () => setSee(!see) }
-						>
-              { seeMoney(see) }
-            </button>
-        </section>
+        <button
+          type="button"
+          className="button-icon container-money"
+          onClick={ () => setSee(!see) }
+        >
+          { seeMoney(see) }
+        </button>
+      </section>
     </header>
   );
 }
