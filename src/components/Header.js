72beafada9dee98e375082016990f1ff9722f28b
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2022-01-29 21:20:42 -0300

    Inserir - Header.js - Criação da estrutura básica do header, inclusão do icones do olho, ajuda e logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import verSenha from "../assets/Archive/eye_idle.png";
+import naoVerSenha from "../assets/Archive/eye_off_idle.png";
+import help from "../assets/Archive/help_idle.png";
+import logout from "../assets/Archive/logout_idle.png"
 
 export default function Header() {
   return (
     <header className='header'>
-      <h1>Hello world</h1>
+        <section className='header-container-left'>
+            <img src={ logout } alt="Sair da conta" width={"50px"} />
+            <img src={ help } alt="Ajuda" width={"50px"} />
+        </section>
       <p>Estamos online (comemore) </p>
-      <p>TRABALHANDO EM TEMPO REAL</p>
+      <section className='header-container-right'>
+            <h3>$ </h3>
+            <img src={ verSenha } alt="Dinheiro" width={ "35px" }/>
+        </section>
     </header>
   );
 }
