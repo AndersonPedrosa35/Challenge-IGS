6d67de5a0ae1edbb87a3c499a5ddd1f45fa396c8
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2022-01-29 21:46:24 -0300

    Inserir - Styles - Estilização dos componentes, e iniciando logica para aparecer e bloquear dinheiro

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import verSenha from "../assets/Archive/eye_idle.png";
 import naoVerSenha from "../assets/Archive/eye_off_idle.png";
 import help from "../assets/Archive/help_idle.png";
 import logout from "../assets/Archive/logout_idle.png"
 
+function seeMoney(state) {
+    if (state === true) {
+        return false;
+    }
+}
+
 export default function Header() {
+    const [setSee, see] = useState(false);
+
   return (
     <header className='header'>
         <section className='header-container-left'>
@@ -14,7 +22,11 @@ export default function Header() {
       <p>Estamos online (comemore) </p>
       <section className='header-container-right'>
             <h3>$ </h3>
-            <img src={ verSenha } alt="Dinheiro" width={ "35px" }/>
+            {/* <button
+              type="button"
+              onClick={ () => setSee(!see) }>
+            </button> */}
+						<img src={ verSenha } alt="Dinheiro" width={ "35px" }/>
         </section>
     </header>
   );
