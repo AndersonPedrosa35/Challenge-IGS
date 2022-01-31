90a5a5e191ebecdb90ad2a71e8d171b3ed67c985
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2022-01-31 14:59:50 -0300

    Inserir - Components & styles - Criação do footer, mudança nos seletores next e previous do carrossel, add mais um imagem, refatorando o código

--- src/components/Carrosel.js
+++ src/components/Carrosel.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
 import ListCards from './ListCards';
+import next from '../assets/Archive/arrow_right.png';
+import previous from '../assets/Archive/arrow_left.png';
+import indicatorOn from '../assets/Archive/pagination_on.png';
 
 
 export default function Carrosel() {
   return (
-    <Carousel>
+    <Carousel
+      nextIcon={ <img src={ next } alt="Proximo" width={ "50px" } /> }
+      prevIcon={ <img src={ previous } alt="Proximo" width={ "50px" } /> }
+      nextLabel={ "Proximo" }
+      prevLabel={ "Anteriors"}
+    >
       <Carousel.Item>
-        <section className="container-cards">
-          <ListCards />
-        </section>
+        <ListCards />
       </Carousel.Item>
       <Carousel.Item>
-        <section className="container-cards">
-          <ListCards />
-        </section>
+        <ListCards />
       </Carousel.Item>
       <Carousel.Item>
-        <section className="container-cards">
-          <ListCards />
-        </section>
+        <ListCards />
       </Carousel.Item>
     </Carousel>
   );
